2d79febcd3a436847f6d66ec54d8b19caa118736
pfeldman <pablofeldman90@gmail.com>
2025-09-30 11:52:52 -0300

    Initial Commit with next+react app

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-center">Digioh React Next Test Page</h1>
+    </div>
+  );
+}
